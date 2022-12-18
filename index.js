4f98dfe1e7364c601882b7d13533422a75a2c6a1
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-12-18 16:04:55 +0700

    ขั้นตอนการทำงานของตัวแปลภาษา Java

---
+++ index.js
@@ -0,0 +1,15 @@
+/* 
+ขั้นตอนการทำงานของตัวแปลภาษา Java
+
+Editor -> Source File (.java) -> Compiler -> Class File (.class)
+
+Java application
+Library Files -> Interpreter -> Running Program
+
+๋Java compiler คือ ตัวคอมไฟเลอร์จะแปลงไฟล์ source code ที่มีนามสกุล .java
+ให้อยู่ในรูปของ bytecode เป็นไฟล์นามสกุล .class (หลังจากคอมไฟล์ Hello.java แล้ว
+จะได้ไฟล์ Hello.class ในไดเร็คเทอรีเดียวกันกับไฟล์ Hello.java)
+
+๋JVM จะจัดการแปลง bytecode ให้เป็นคำสั่งภาษาเครื่องที่เหมาะสม 
+
+*/
